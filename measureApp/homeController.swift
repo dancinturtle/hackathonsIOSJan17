08c36e5307ebe27c0af59fe3c2fb0eda1d329360
Ken Bains <kennbains@gmail.com>
2017-01-13 14:25:23 -0800

    initial

---
+++ measureApp/homeController.swift
@@ -0,0 +1,26 @@
+//
+//  homeController.swift
+//  measureApp
+//
+//  Created by ken bains on 1/12/17.
+//  Copyright © 2017 ken bains. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class homeController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "homebackground.png")!)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ measureApp/welcomeController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  projectMeasure
+//
+//  Created by ken bains, Grace Shieh, and Joe Casto Abundo Buttram on 1/12/17.
+//  Copyright © 2017 ken bains. All rights reserved.
+//
+import UIKit
+import CoreMotion
+class welcomeController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
